7d28f59cbd6bd3cc0d8c87cb1c9bf74d436f2d2e
code34 <nicolas_boiteux@yahoo.fr>
2018-02-06 23:45:28 +0100

    delete useless variable

--- FACILE/chucknorris.go
+++ FACILE/chucknorris.go
@@ -20,25 +20,21 @@ func main() {
 
 	var newmessage string
 	var bits string
-	var change bool;
-	var unary int32;
+	var prev int32;
 
 	for _,letter := range message {
-		bits = fmt.Sprintf("%7b", letter);
+		bits = fmt.Sprintf("%.7b", letter);
 		for _,bit := range bits {
-			if!(bit == unary) { 
-				unary = bit
-				change = true
-			}
-			if(change) {
-				if(bit == 49) {
-					newmessage = newmessage + " 0 "
+			if(bit == prev) { 
+			    newmessage = newmessage + "0"
+			} else {
+			    if(bit == 49) {
+				    newmessage = newmessage + " 0 0"
 				} else {
-					newmessage = newmessage + " 00 "
+					newmessage = newmessage + " 00 0"
 				}
-				change = false;
+				prev = bit
 			}
-			newmessage = newmessage + "0"
 		}
 	}
 	newmessage = newmessage[1:len(newmessage)]
