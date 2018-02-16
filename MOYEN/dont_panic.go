c04bc99ee66e9eb76a6e7aa9e903d5ce6d26a97c
code34 <nicolas_boiteux@yahoo.fr>
2018-02-16 23:44:17 +0100

    optimisations

--- MOYEN/dont_panic.go
+++ MOYEN/dont_panic.go
@@ -2,33 +2,12 @@ package main
 
 import "fmt"
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
-
-
-
 func main() {
-	// nbFloors: number of floors
-	// width: width of the area
-	// nbRounds: maximum number of rounds
-	// exitFloor: floor on which the exit is found
-	// exitPos: position of the exit on its floor
-	// nbTotalClones: number of generated clones
-	// nbAdditionalElevators: ignore (always zero)
-	// nbElevators: number of elevators
 	var nbFloors, width, nbRounds, exitFloor, exitPos, nbTotalClones, nbAdditionalElevators, nbElevators int
 	fmt.Scan(&nbFloors, &width, &nbRounds, &exitFloor, &exitPos, &nbTotalClones, &nbAdditionalElevators, &nbElevators)
 
-	var floordone int = -1
-	var elevators map[int]int
-	elevators = make(map[int]int)
-
-	
+	elevators := make([]int, nbElevators+1)    
 	for i := 0; i < nbElevators; i++ {
-		// elevatorFloor: floor on which this elevator is found
-		// elevatorPos: position of the elevator on its floor
 		var elevatorFloor, elevatorPos int
 		fmt.Scan(&elevatorFloor, &elevatorPos)
 		elevators[elevatorFloor] = elevatorPos
@@ -36,9 +15,6 @@ func main() {
 	
 
 	for {
-		// cloneFloor: floor of the leading clone
-		// clonePos: position of the leading clone on its floor
-		// direction: direction of the leading clone: LEFT or RIGHT
 		var cloneFloor, clonePos int
 		var direction string
 		fmt.Scan(&cloneFloor, &clonePos, &direction)
@@ -53,10 +29,9 @@ func main() {
 		}
 
 		var action = "WAIT"
-		if floordone < cloneFloor {
+		if cloneFloor > -1 {
 			if clonePos > dirPos && direction == "RIGHT" { action = "BLOCK" }
 			if clonePos < dirPos && direction == "LEFT" { action = "BLOCK" } 
-			floordone += 1
 		}
 		
 		fmt.Println(action) // action: WAIT or BLOCK
