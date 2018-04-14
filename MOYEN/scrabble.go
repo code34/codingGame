fc88a4f75317359dd3f519703ccb593936901756
code34 <nicolas_boiteux@yahoo.fr>
2018-04-14 11:10:04 +0200

    first test success

--- MOYEN/scrabble.go
+++ MOYEN/scrabble.go
@@ -11,7 +11,7 @@ func main() {
 	scanner.Buffer(make([]byte, 1000000), 1000000)
 
 	var words [][]string
-	var solution [][]string
+	var solution string
 	var N int
 	scanner.Scan()
 	fmt.Sscan(scanner.Text(),&N)
@@ -26,21 +26,36 @@ func main() {
 	scanner.Scan()
 	letters := strings.Split(scanner.Text(), "")
 
+	max := 0
 	for _,word := range words {
 		index := 0
 		for _,letter := range letters {
 			for _, letterofword := range word {
 				if letter == letterofword {
-					index++
+					if letter == "e" || letter == "a" || letter == "i" || letter == "o" || letter == "n" || letter == "r" || letter == "t" || letter == "l" || letter == "s" || letter == "u" {
+						index++
+					} else if letter == "d" || letter == "g" {
+						index = index + 2
+					} else if letter == "b" || letter == "c" || letter == "m" || letter == "p" {
+						index = index + 3
+					} else if letter == "f" || letter == "h" || letter == "v" || letter == "w" || letter == "y" {
+						index = index + 4
+					} else if letter == "k" {
+						index = index + 5
+					} else if letter == "j" || letter == "x" {
+						index = index + 8
+					} else if letter == "q" || letter == "z" {
+						index = index + 10
+					}
 				}
 			}
 		}
-		if index > 0 {
-			solution = append(solution, word)
+		if index > max {
+			solution = strings.Join(word, "")
+			max = index
 		}
 		fmt.Fprintf(os.Stderr, "%d %d %s\n", len(letters), index, word)
 	}
-	fmt.Fprintf(os.Stderr, "%s \n", solution)
-	fmt.Fprintf(os.Stderr, "%s \n", letters)
-	fmt.Println("non")// Write answer to stdout
+	//fmt.Fprintf(os.Stderr, "%s \n", solution)
+	fmt.Println(solution)// Write answer to stdout
 }
