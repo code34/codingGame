665c8c60a97dd627ad8e02ceeb77617808183783
code34 <nicolas_boiteux@yahoo.fr>
2018-04-15 00:01:04 +0200

    optimized solution :)

--- MOYEN/scrabble.go
+++ MOYEN/scrabble.go
@@ -1,10 +1,11 @@
+/*Lorsqu'on joue au Scrabble©, chaque joueur tire 7 lettres et doit trouver le mot qui permet d'obtenir le meilleur score en utilisant ces lettres. Un joueur n'a pas l'obligation de former un mot de 7 lettres, le mot peut être plus court. La seule contrainte est que le mot ne peut être constitué que des lettres que le joueur a tiré. Par exemple, avec les lettres etaenhs, les mots possibles (en anglais) sont : ethane, hates, sane, ant. Votre objectif est de trouver le mot qui marque le plus de points en utilisant les lettres disponibles (1 à 7 lettres).*/
+
 package main
 
 import "fmt"
 import "os"
 import "bufio"
 import "strings"
-//import "strconv"
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
@@ -25,37 +26,40 @@ func main() {
 	
 	scanner.Scan()
 	letters := strings.Split(scanner.Text(), "")
+	fmt.Fprintf(os.Stderr, "%s \n", letters)
 
-	max := 0
+	scoremax := 0
 	for _,word := range words {
-		index := 0
+		score := 0
+		lettercount := 0
 		for _,letter := range letters {
 			for _, letterofword := range word {
 				if letter == letterofword {
 					if letter == "e" || letter == "a" || letter == "i" || letter == "o" || letter == "n" || letter == "r" || letter == "t" || letter == "l" || letter == "s" || letter == "u" {
-						index++
+						score++
 					} else if letter == "d" || letter == "g" {
-						index = index + 2
+						score = score + 2
 					} else if letter == "b" || letter == "c" || letter == "m" || letter == "p" {
-						index = index + 3
+						score = score + 3
 					} else if letter == "f" || letter == "h" || letter == "v" || letter == "w" || letter == "y" {
-						index = index + 4
+						score = score + 4
 					} else if letter == "k" {
-						index = index + 5
+						score = score + 5
 					} else if letter == "j" || letter == "x" {
-						index = index + 8
+						score = score + 8
 					} else if letter == "q" || letter == "z" {
-						index = index + 10
+						score = score + 10
 					}
+					lettercount++
+					break
 				}
 			}
 		}
-		if index > max {
+		if score > scoremax && len(word) <= lettercount {
 			solution = strings.Join(word, "")
-			max = index
+			scoremax = score
 		}
-		fmt.Fprintf(os.Stderr, "%d %d %s\n", len(letters), index, word)
+		fmt.Fprintf(os.Stderr, "%d %d %s %d %d\n", len(letters), score, word, len(word), lettercount)
 	}
-	//fmt.Fprintf(os.Stderr, "%s \n", solution)
 	fmt.Println(solution)// Write answer to stdout
 }
