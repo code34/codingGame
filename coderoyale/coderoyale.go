4b29430f43a35d7c26f18beeefc6f2b49cf5aaee
code34 <nicolas_boiteux@yahoo.fr>
2018-04-20 19:17:31 +0200

    add logs !

--- coderoyale/coderoyale.go
+++ coderoyale/coderoyale.go
@@ -1,12 +1,7 @@
 package main
 
 import "fmt"
-//import "os"
-
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
+import "os"
 
 func main() {
 	var numSites int
@@ -28,6 +23,7 @@ func main() {
 			// owner: -1 = No structure, 0 = Friendly, 1 = Enemy
 			var siteId, ignore1, ignore2, structureType, owner, param1, param2 int
 			fmt.Scan(&siteId, &ignore1, &ignore2, &structureType, &owner, &param1, &param2)
+			fmt.Fprintf(os.Stderr, "input:  siteid:%d ignore1:%d ignore2:%d structureType:%d owner:%d param1:%d param2:%d \n", siteId, ignore1, ignore2, structureType, owner, param1, param2)
 		}
 		var numUnits int
 		fmt.Scan(&numUnits)
@@ -36,10 +32,10 @@ func main() {
 			// unitType: -1 = QUEEN, 0 = KNIGHT, 1 = ARCHER
 			var x, y, owner, unitType, health int
 			fmt.Scan(&x, &y, &owner, &unitType, &health)
+			fmt.Fprintf(os.Stderr, "units: x:%d y:%d owner:%d unitType:%d health:%d  \n", x, y, owner, unitType, health)
 		}
 		
 		// fmt.Fprintln(os.Stderr, "Debug messages...")
-		
 		// First line: A valid queen action
 		// Second line: A set of training instructions
 		fmt.Println("WAIT")
