c9a92cf47f266c4dd24c20f1d801009145d71196
code34 <nicolas_boiteux@yahoo.fr>
2018-04-20 20:10:28 +0200

    add structures

--- coderoyale/coderoyale.go
+++ coderoyale/coderoyale.go
@@ -3,13 +3,27 @@ package main
 import "fmt"
 import "os"
 
+type site struct{
+	siteId, x, y, radius, ignore1, ignore2, structureType, owner, param1, param2 int
+}
+
+type unit struct {
+	x, y, owner, unitType, health int
+}
+
 func main() {
 	var numSites int
 	fmt.Scan(&numSites)
+	allsites := make(map[int]site)
 	
 	for i := 0; i < numSites; i++ {
 		var siteId, x, y, radius int
 		fmt.Scan(&siteId, &x, &y, &radius)
+		value := allsites[siteId]
+		value.x = x
+		value.y = y
+		value.radius = radius
+		allsites[siteId] = value
 	}
 	for {
 		// touchedSite: -1 if none
@@ -23,11 +37,18 @@ func main() {
 			// owner: -1 = No structure, 0 = Friendly, 1 = Enemy
 			var siteId, ignore1, ignore2, structureType, owner, param1, param2 int
 			fmt.Scan(&siteId, &ignore1, &ignore2, &structureType, &owner, &param1, &param2)
+			value := allsites[siteId]
+			value.ignore1 = ignore1
+			value.ignore2 = ignore2
+			value.structureType = structureType
+			value.owner = owner
+			value.param1 = param1
+			value.param2 = param2
+			allsites[siteId] = value
 			fmt.Fprintf(os.Stderr, "input:  siteid:%d ignore1:%d ignore2:%d structureType:%d owner:%d param1:%d param2:%d \n", siteId, ignore1, ignore2, structureType, owner, param1, param2)
 		}
 		var numUnits int
 		fmt.Scan(&numUnits)
-		
 		for i := 0; i < numUnits; i++ {
 			// unitType: -1 = QUEEN, 0 = KNIGHT, 1 = ARCHER
 			var x, y, owner, unitType, health int
@@ -38,7 +59,11 @@ func main() {
 		// fmt.Fprintln(os.Stderr, "Debug messages...")
 		// First line: A valid queen action
 		// Second line: A set of training instructions
-		fmt.Println("WAIT")
+		if touchedSite != -1 {
+			fmt.Fprintf(os.Stdout,"BUILD %d BARRACKS-ARCHER \n", touchedSite)
+		} else {
+			fmt.Println("WAIT")
+		}
 		fmt.Println("TRAIN")
 	}
 }
