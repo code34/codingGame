45d23b24341610147921b9040f8cab5f75a23445
code34 <nicolas_boiteux@yahoo.fr>
2018-05-13 19:47:29 +0200

    pass the 1 test

--- DIFFICILE/lelabyrinthe.go
+++ DIFFICILE/lelabyrinthe.go
@@ -3,11 +3,6 @@ package main
 import "fmt"
 import "os"
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
-
 type element struct {
 	x int
 	y int
@@ -15,11 +10,13 @@ type element struct {
 
 type themap struct {
 	mymap [200][100]string
+	teleport element
+	controler element
 }
 
 func distance (source element, destination element) int {
-	newx := destination.x - destination.y
-	newy := source.x - source.y
+	newx := destination.x - source.x
+	newy := destination.y - source.y
 	if newx < 0 {
 		newx = newx * -1
 	}
@@ -29,16 +26,41 @@ func distance (source element, destination element) int {
 	return (newx+newy)
 }
 
+func (carte *themap) defineGoal(position element) element {
+	var goal element
+	if carte.controler.x == -1 && carte.controler.y == -1 {
+		goal = element {position.x +5 , position.y}
+	} else {
+		goal = carte.controler
+	}
+	return goal
+}
+
 func (carte *themap) findNextMove (position element) element {
 	var result element
+	var goal element
+	min := 20000
 	cross := []element { {position.x - 1, position.y}, {position.x, position.y - 1}, {position.x + 1, position.y}, {position.x, position.y +1}}
+	goal = carte.defineGoal(position)
 	for _, neighbour := range cross {
-		if carte.mymap[neighbour.x][neighbour.y] != "#" {
+		if carte.mymap[neighbour.x][neighbour.y] != "#" && distance(element{neighbour.x, neighbour.y}, goal) < min {
+			min = distance(element{neighbour.x, neighbour.y}, goal)
 			result = neighbour
-			fmt.Fprintf(os.Stderr, "KIRK: %d\n", result)
 		}
 	}
 	return result
+}
+
+func (carte *themap) findDirection (source element, target element) string {
+	if source.x > target.x {
+		return "LEFT"
+	} else if source.x < target.x {
+		return "RIGHT"
+	} else if source.y > target.y {
+		return "UP"
+	} else {
+		return "DOWN"
+	}
 }
 
 func main() {
@@ -49,10 +71,10 @@ func main() {
 	var R, C, A int
 	var carte themap
 	var path []element
+	reverse := false
 	kirk := element{-1,-1}
-	start := element{-1,-1}
-	end := element{-1,-1}
-	goal := end
+	carte.teleport = element{-1,-1}
+	carte.controler = element{-1,-1}
 	fmt.Scan(&R, &C, &A)
 
 	for {
@@ -65,15 +87,13 @@ func main() {
 			var ROW string
 			fmt.Scan(&ROW)
 			for x, value  := range ROW {
-				if start.x == -1 && string(value) == "T" {
-					start.x = x
-					start.y = y
+				if carte.teleport.x == -1 && string(value) == "T" {
+					carte.teleport.x = x
+					carte.teleport.y = y
 				}
-				if end.x == -1 && string(value) == "C" {
-					end.x = x
-					end.y = y
-					goal.x = x
-					goal.y = y
+				if carte.controler.x == -1 && string(value) == "C" {
+					carte.controler.x = x
+					carte.controler.y = y
 				}
 				if y == kirk.y && x == kirk.x {
 					fmt.Fprintf(os.Stderr, "K")
@@ -84,14 +104,18 @@ func main() {
 				}
 			}
 			fmt.Fprintf(os.Stderr, "\n")
-			if end == kirk {
-				goal = start
-			}
 		}
-		path = append(path, kirk)
 		nextmove:= carte.findNextMove (kirk)
-		fmt.Fprintf(os.Stderr, "%d %d %d %d %d\n", kirk, start, end, goal, path)
-		fmt.Fprintf(os.Stderr, "NEXTMOVE: %d\n", nextmove)
-		fmt.Println("RIGHT") // Kirk's next move (UP DOWN LEFT or RIGHT).
+		if kirk == carte.controler || reverse {
+			reverse = true
+			nextmove = path[len(path)-1]
+			path = path[:len(path)-1]
+		} else {
+			path = append(path, kirk)
+		}
+		direction:= carte.findDirection(kirk, nextmove)
+		fmt.Fprintf(os.Stderr, "%d %d %d\n", carte.teleport, carte.controler, path)
+		fmt.Fprintf(os.Stderr, "NEXTMOVE: %d KIRK: %d\n", nextmove, kirk)
+		fmt.Println(direction) // Kirk's next move (UP DOWN LEFT or RIGHT).
 	}
 }
